b0227935b65700403c832c062634feb442cf8c7a
filipradenovic <fradenovic@gmail.com>
2022-06-06 14:46:55 -0700

    [nbm-spam] LICENSE, CODE_OF_CONDUCT, CONTRIBUTING, and setup
    [ghstack-poisoned]

---
+++ setup.py
@@ -0,0 +1,29 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import setuptools
+
+
+with open("README.md", encoding="utf8") as f:
+    readme = f.read()
+
+DISTNAME = "nbm_spam"
+VERSION = "0.1"
+DESCRIPTION = "NeurIPS 2022 Supplementary Code"
+LONG_DESCRIPTION = readme
+AUTHOR = "Anonymous"
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name=DISTNAME,
+        packages=setuptools.find_packages(),
+        version=VERSION,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        author=AUTHOR,
+        setup_requires=["pytest-runner"],
+        test_requires=["pytest"],
+    )
